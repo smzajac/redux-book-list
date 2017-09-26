a5f05889f87f35de11c09b4e4ce18e5052b54377
Sarah Zajac <sarahmzajac@gmail.com>
2017-09-26 14:54:44 -0400

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+const BOOK_SELECTED = "BOOK_SELECTED";
+//select book is an action creator, it will need an action type
+// export function selectBook(book) {
+//
+//   return {
+//     type: BOOK_SELECTED,
+//     payload: book
+//     //You should create a Type and Payload for your Action here
+//   };
+// }
+
+export const selectBook = book => {
+  return {
+    type: BOOK_SELECTED,
+    payload: book
+  };
+};
